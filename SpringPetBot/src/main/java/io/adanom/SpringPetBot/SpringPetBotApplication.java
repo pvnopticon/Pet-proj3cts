473c932e28dadf409a6b6cdf720fa635e82ac609
Monada <teegreessq@gmail.com>
2024-05-23 03:54:54 +0300

    initial commit

---
+++ SpringPetBot/src/main/java/io/adanom/SpringPetBot/SpringPetBotApplication.java
@@ -0,0 +1,13 @@
+package io.adanom.SpringPetBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetBotApplication.class, args);
+	}
+
+}
---
+++ SpringPetBot/src/test/java/io/adanom/SpringPetBot/SpringPetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.adanom.SpringPetBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
